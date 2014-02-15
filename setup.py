812615b9d8420e0e8c94f37e431b8cb027afac58
Adam Fourney <adam.fourney@gmail.com>
2014-02-15 08:29:02 -0500

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyra',
-    version='0.2.5dev',
+    version='0.2.6dev',
     description="A python implementation of the GCL region algebra and query language described by Clarke et al.",
     author = "Adam Fourney",
     url = "http://github.com/afourney/pyra",
